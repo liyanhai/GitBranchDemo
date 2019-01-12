49f453bffc62808a764dc4257388056e14845b8d
lyh_glb <lyh_glb@atguigu.com>
2019-01-12 17:23:02 +0800

    新建了分支并创建了Customer对象

---
+++ src/com/lyh/git/Customer.java
@@ -0,0 +1,21 @@
+package com.lyh.git;
+
+/**
+ * @author liyanhai
+ * @create 2019-01-12 17:22
+ */
+public class Customer {
+
+    public String getCusName() {
+        return cusName;
+    }
+
+    public void setCusName(String cusName) {
+        this.cusName = cusName;
+    }
+
+    private String cusName;
+
+
+
+}
